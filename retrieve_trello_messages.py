55fbbf43a0d643f4d6cbf6580551cdf664ad4f76
Jonathan Pleban <opleban@gmail.com>
2019-01-14 00:42:39 -0500

    Updates with current student sprint and user story boards

--- retrieve_trello_messages.py
+++ retrieve_trello_messages.py
@@ -3,8 +3,9 @@ import json
 import argparse
 import os
 
-team_boards = ['5a5000b1aec1a269f123a949', '5a4fa7ca9b9444c95a7b5725', '5a4fa7a4694787234e702a84', '5a4cf20858260d304a47b9d1',
-'5a5000814017ad42e45d6961', '5a5017ab83962a8204320c0e', '5a50009f6cff3bf4290ef839', '5a4fa8aa80b013868bbe03cf']
+team_boards = ['5c377c57d55dc2316170ba48', '5c37638faea70f1ce1fe97ef', '5c377c5745d2916e572cb4e7', '5c377c5ac950f37ab1f855a6', '5c37811c19d071436041d2ed',
+'5c362a4c05e14d4511f677a5', '5c33d6c4d559f019e14cf451', '5c361a3e02ecb684511a4dad', '5c362a4da2945e78b830728d', '5c362aee39b3cb8bae066f92', '5c36142c81f94882f08a921a',
+'5c362a469ccde91a98d37b22', '5c36132d054082518e2a33ae','5c37635e2df5d87f625aad97', '5c376384382b4c7b8b3977a9', '5c37630d47cf330c9466cda9']
 
 key = os.environ['TRELLOKEY']
 token = os.environ['TRELLOTOKEN']
@@ -17,7 +18,7 @@ def get_action_data():
 	for board in team_boards:
 		board_response = requests.get(base_url + board, params = url_params)
 		board_response_data = json.loads(board_response.text)
-		
+
 		board_data = {}
 		board_data['name'] = board_response_data['name']
 		print('Retrieving data from ' + board_data['name'] + '...')
@@ -32,16 +33,16 @@ def get_action_data():
 		for action in actions_response_data:
 			member = action['memberCreator']['fullName']
 			board_data['members'].add(member)
-			if 'list' in action['data'].keys():
-				action_name = action['data']['list']['name']            
+			if 'list' in action['data'].keys() and 'name' in action['data']['list'].keys():
+				action_name = action['data']['list']['name']
 			elif 'card' in action['data'].keys():
 				action_name = action['data']['card']['name']
 			else:
 				action_name = None
 			board_data['actions'].append({'member':member, 'type':action['type'], 'action_name':action_name, 'time':action['date'][:10]})
-			
+
 		print(len(board_data['actions']))
-		board_data['members'] = list(board_data['members']) 
+		board_data['members'] = list(board_data['members'])
 		data[board] = board_data
 	return data
 
